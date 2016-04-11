b9b9586217334e89de74ea39368ae4b0d490aaaa
jnb2387 <jnb2387@gmail.com>
2016-04-11 23:14:46 +0000

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("age").addEventListener('click', function(){
+    document.getElementById("age").innerHTML=24;
+});
+
+document.getElementById("please").addEventListener('click', function(){
+    document.getElementById("please").innerHTML = "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpLOVUyCVXzbpbavh8JVI8cbLp1IgZCU2PwNeVgjHxSykEzjJNyPAW9eqg'>"
+});
+
+document.getElementById("hobbies").addEventListener('mouseover', function(){
+    document.getElementById("hobbies").innerHTML = "Hiking, Scuba Diving, Dogs"
+});
+
+document.querySelector("button").addEventListener('click', function(){
+    document.body.style.backgroundColor="dodgerblue"});
+    
+document.getElementById("height").addEventListener('click', function(){
+    document.getElementById("height").innerHTML= "wow"
+})
